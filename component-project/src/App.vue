aebdafd600d7a960c3c9c247d22c22b6e3d35187
Savira83 <pekaryuk.ira@gmail.com>
2022-08-17 23:11:07 +0300

    try to do first component

---
+++ component-project/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <TaskList v-for='category  in categories'
+  :key = 'category.id'
+  :name = 'category.name'
+  @add-category='addNewCategory'
+  ></TaskList>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TaskList
+  },
+  data (){
+    return{
+      categories:       
+        [
+          {
+          id:1,
+          name: 'category 1',
+          tasks: [{
+              
+              taskName: 'Task 1.1',
+              status: false,
+              },
+              {
+              
+              taskName: 'Task 1.2',
+              status: false,
+          }] 
+          },]
+    }
+  },
+  methods:{
+    addNewCategory: function(){ 
+            alert(" nnn");
+     }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ component-project/src/components/TaskList.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<ul>
+			<li>
+				{{name}}
+				<input type="button" value="+" @click="$emit('addCategory')">
+			</li>
+		</ul> 
+	</div>
+</template>
+
+<script>
+export default{
+	props:['id','name'],
+	//emits: ['addCategory']
+
+
+}
+
+	
+</script>
